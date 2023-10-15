236d08e6df7ca70f0935747e7bcf2037e9b2ae70
AnilKumar Mishra <akmsdet@gmail.com>
2023-10-15 23:02:44 +0530

    updated

--- src/main/java/common/JavaScriptMethods.java
+++ src/main/java/common/JavaScriptMethods.java
@@ -9,4 +9,19 @@ public class JavaScriptMethods {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         return js.executeScript("return document.title;").toString();
     }
+
+ public void scrollByJS(WebDriver driver, int xValue, int yValue) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(xValue,yValue)","");
+    }
+
+ public void scrollToElementByJS(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",element);
+    }
+
+ public void scrollToBottomPagetByJS(WebDriver driver, WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy("0,document.body.scrollHeight)");
+    }
 }
