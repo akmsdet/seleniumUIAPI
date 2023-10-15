d9ac3ec9c159774e7d7517d556f13e384ea6efd6
AnilKumar Mishra <akmsdet@gmail.com>
2023-10-16 02:04:57 +0530

    updated few changes

--- src/test/java/uitest/ChromeUpdatedSetup.java
+++ src/test/java/uitest/ChromeUpdatedSetup.java
@@ -1,5 +1,6 @@
-package ui_chrome115;
+package uitest;
 
+import common.JavaScriptMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -7,19 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-public class Chrome115Setup {
+public class ChromeUpdatedSetup {
     WebDriver driver = null;
 
     @Test
     public void testChrome115() throws InterruptedException {
-  ChromeOptions options = new ChromeOptions();      
-/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\anil.k.mishra\\seleniumDrivers\\chromedriver-win64\\chromedriver.exe");
-        
+        ChromeOptions options = new ChromeOptions();
+/*
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\anil.k.mishra\\seleniumDrivers\\chromedriver-win64\\chromedriver.exe");
         options.setBinary("C:\\Users\\anil.k.mishra\\seleniumDrivers\\chrome-win64\\chrome.exe");
-
         options.addArguments("--user-data-dir=C:\\Users\\anil.k.mishra\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1");
-*/        
-	options.addArguments("--no-sandbox");
+*/
+        options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("incognito");
@@ -29,6 +29,9 @@ public class Chrome115Setup {
         driver.get("https://google.co.in");
         driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
         Thread.sleep(5000);
+        for (int i = 0; i <= 1000; i = i + 10) {
+            JavaScriptMethods.scrollByJS(driver, 0, i);
+        }
         driver.quit();
     }
 
